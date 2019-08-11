b7c9387eb3f9cd2c3996d222e62d93a4e0e2cf13
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2019-08-11 18:49:01 +1000

    angular: cmp-databinding-start: add alias to Output component

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
   constructor() { }
 
   ngOnInit() {
