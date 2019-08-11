a50a778ff388ee02164792b2552251b94b99332e
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2019-08-11 19:09:32 +1000

    angular: cmp-databinding-start: remove encapsulation not needed for this app

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {name: string, type: string, content: string};
